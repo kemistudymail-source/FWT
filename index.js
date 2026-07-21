db391c8f10fe1de55b11eff363f9ecdcba3e9652
kemistudymail-source <kemi.study.mail@gmail.com>
2026-07-21 17:22:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require("path");
 const http = require("http");
 
 // ── Config ────────────────────────────────────────────────────────────────────
-const HELIUS_API_KEY = "7cda4855-82e2-45be-843a-ba196aebf3ce";
+const HELIUS_API_KEY = "7705bf73-6fa1-4477-b1b0-0472ded87578";
 const HELIUS_RPC = `https://mainnet.helius-rpc.com/?api-key=${HELIUS_API_KEY}`;
 const HELIUS_TX  = `https://api-mainnet.helius-rpc.com/v0`;
 const TELEGRAM_TOKEN = "8769953136:AAHFrooUVd1yx8BxPbJVTJPhthyhW-ptTqY";
